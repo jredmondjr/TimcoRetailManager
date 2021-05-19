043c7d7a45cf4ccae003741028c9780a1c10dd33
Joe Redmond <joeyredmondjr@yahoo.com>
2021-05-19 04:20:38 -0400

    removed allow null

--- TRMData/dbo/Tables/Inventory.sql
+++ TRMData/dbo/Tables/Inventory.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Inventory]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [ProductId] INT NULL, 
+    [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NOT NULL, 
     [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate()
