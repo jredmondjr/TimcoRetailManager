b024a34eba877d4644940ccfcedc1ea88a88ed42
Joe Redmond <joeyredmondjr@yahoo.com>
2021-05-21 01:14:36 -0400

    Added /api/user endpoint

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@id nvarchar(128)
+AS
+Begin
+	SET NOCOUNT ON;
+
+	SELECT Id, FirstName, LastName, EmailAddress, CreatedDate
+	FROM [dbo].[User]
+	where Id = @id
+END
